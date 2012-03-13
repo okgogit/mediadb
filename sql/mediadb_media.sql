198ecbde9f856134ae551e84ae13998bf4db725b
Ethan Miller <ethanzanemiller@okgo.net>
2012-03-12 19:51:11 -0700

    Removed media_path column from mediadb_media table.

--- sql/mediadb_media.sql
+++ sql/mediadb_media.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS `<?php echo $wpdb->prefix;?>mediadb_media` (
   `media_id` varchar(255) NOT NULL,
   `filename` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `media_path` varchar(255) NOT NULL,
   PRIMARY KEY (`media_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
